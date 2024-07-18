56334e6865ed0a119dcca1b0a8a8cdff0feaecf7
Noor Nabi <gcnoor6@gmail.com>
2024-07-18 11:39:36 +0500

    patterns

--- patterns/index.cpp
+++ patterns/index.cpp
@@ -11,6 +11,16 @@ class Patterns {
         void hollowRectangle();
         Patterns(int n);
         void invertedHalfPyramid();
+        void halfPyramin180Rotate();
+        void halfPyraminUsingNumber();
+        void floyidTriangle();
+        void butterflyPattern();
+        void invertedPattern();
+        void oddEvenPattern();
+        void rhombusPattern();
+        void numberPattern();
+        void palindromePattern();
+        void starPattern();
 
 };
 
@@ -23,7 +33,17 @@ int main() {
     int n;
     cin >> n;
     Patterns P(n);
-    P.invertedHalfPyramid();
+    // P.invertedHalfPyramid();
+    // P.halfPyramin180Rotate();
+    // P.halfPyraminUsingNumber();
+    // P.floyidTriangle();
+    // P.butterflyPattern();
+    // P.invertedPattern();
+    // P.oddEvenPattern();
+    // P.rhombusPattern();
+    // P.numberPattern();
+    // P.palindromePattern();
+    P.starPattern();
     return 0;
 }
 Patterns::Patterns(int rows,int cols){
@@ -60,5 +80,145 @@ void Patterns::invertedHalfPyramid(){
             cout<<"* ";
         }
         cout<<endl;
+    }
+}
+void Patterns::halfPyramin180Rotate(){
+    for(int i=1;i<=this->n;i++){
+        for(int j=1;j<=this->n;j++){
+            if(j <= this->n - i)
+                cout<<"  ";
+            else
+                cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+void Patterns::halfPyraminUsingNumber(){
+    for(int i=1;i<=this->n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
+void Patterns::floyidTriangle(){
+    int count =1;
+    for(int i=1;i<=this->n;i++){
+        for(int j=0;j<i;j++){
+            cout<<count++<<" ";
+        }
+        cout<<endl;
+    }
+}
+void Patterns::butterflyPattern(){
+    for(int i=1;i<=this->n;i++){
+        for(int j=0;j<i;j++){
+                cout<<"* ";
+        }
+        int space = 2*n-2*i;
+        for(int j=0;j<space;j++){
+            cout<<"  ";
+        }
+        for(int j=0;j<i;j++){
+                cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i=this->n;i>0;i--){
+        for(int j=0;j<i;j++){
+                cout<<"* ";
+        }
+        int space = 2*n-2*i;
+        for(int j=0;j<space;j++){
+            cout<<"  ";
+        }
+        for(int j=0;j<i;j++){
+                cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+void Patterns::invertedPattern(){
+    for(int i=this->n;i>0;i--){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+void Patterns::oddEvenPattern(){
+    for(int i=1;i<=this->n;i++){
+        for(int j=0;j<i;j++){
+            if((i+j)%2!= 0)
+                cout<<1<<" ";
+            else
+                cout<<0<<"  ";
+        }
+        cout<<endl;
+    }
+}
+void Patterns::rhombusPattern(){
+    for(int i=0;i<this->n;i++){
+        for(int j=0;j<this->n-i;j++){
+            cout<<"  ";
+        }
+        for(int j=0;j<this->n;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+void Patterns::numberPattern(){
+    for(int i=1;i<=this->n;i++){
+        for(int j=1;j<=this->n-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+void Patterns::palindromePattern(){
+    for(int i = 1; i <= this->n; i++) {
+        int spaces = (this->n - i) * 2;
+        for(int j = 1; j <= spaces; j++) {
+            cout << " ";
+        }
+        for(int j = i; j >= 1; j--) {
+            cout << j << " ";
+        }
+        for(int j = 2; j <= i; j++) {
+            cout << j<<" ";
+        }
+        cout << endl;
+    }
+}
+void Patterns::starPattern(){
+    for(int i = 1; i <= this->n; i++) {
+        int spaces = (this->n - i) * 2;
+        for(int j = 1; j <= spaces; j++) {
+            cout << " ";
+        }
+        for(int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        for(int j = 2; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for(int i = this->n;i>=1; i--) {
+        int spaces = (this->n - i) * 2;
+        for(int j = 1; j <= spaces; j++) {
+            cout << " ";
+        }
+        for(int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        for(int j = 2; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
     }
 }
